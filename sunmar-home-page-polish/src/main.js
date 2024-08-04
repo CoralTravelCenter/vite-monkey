1ad9d2077d2ac86e80940c71c9696132ab4e349f
Mike <mikhail-gankin@yandex.ru>
2024-08-04 13:09:46 +0300

    update css

--- sunmar-home-page-polish/src/main.js
+++ sunmar-home-page-polish/src/main.js
@@ -1,1 +1,11 @@
 import './main.css';
+
+document.addEventListener('b2c_domready', () => {
+	document.querySelectorAll('.heading-tab').forEach(tab => {
+		tab.addEventListener('click', e => {
+			e.target.scrollIntoView(
+				{ behavior: "smooth", block: "nearest", inline: "start" }
+			)
+		})
+	})
+})
