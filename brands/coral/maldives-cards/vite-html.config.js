77d4896016efed6849afb1ef3ab4051fde747b14
Shamber <147848650+laijara@users.noreply.github.com>
2026-07-22 16:47:23 +0300

    builder

---
+++ brands/coral/maldives-cards/vite-html.config.js
@@ -0,0 +1,13 @@
+import {defineConfig} from 'vite';
+import {viteSingleFile} from 'vite-plugin-singlefile';
+
+export default defineConfig({
+  root: "C:\\Users\\ruslan1\\Documents\\01ruslan\\work\\vite-monkey\\brands\\coral\\maldives-cards",
+  plugins: [viteSingleFile()],
+  build: {
+    outDir: 'dist-html',
+    emptyOutDir: true,
+    minify: true,
+    target: 'esnext',
+  }
+});
