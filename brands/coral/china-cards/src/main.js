579ec49334b18ff988a37aa429bd6e0eb727308b
Shamber <147848650+laijara@users.noreply.github.com>
2026-07-24 11:59:28 +0300

    minor fix

--- brands/coral/china-cards/src/main.js
+++ brands/coral/china-cards/src/main.js
@@ -1,7 +1,7 @@
 import { markup } from "./scripts/includeImages.js";
 import './style.css';
 
-const container = document.getElementById('widget-china-card');
+const container = document.getElementById('widget-china-cards');
 
 if (container) {
     container.insertAdjacentHTML('afterbegin', markup);
