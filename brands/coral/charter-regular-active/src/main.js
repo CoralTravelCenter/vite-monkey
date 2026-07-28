6eac85b3147942a98a1a38f978ff3f313aaf01d4
Shamber <147848650+laijara@users.noreply.github.com>
2026-07-28 13:02:02 +0300

    fix charter-regular-active

--- brands/coral/charter-regular-active/src/main.js
+++ brands/coral/charter-regular-active/src/main.js
@@ -1,27 +1,20 @@
 function trackFlightTypeLoad() {
-  const observer = new MutationObserver((mutations, obs) => {
+  const intervalId = setInterval(() => {
     const charterBtn = document.querySelector('[data-testid="ptff-charter-tab-btn"]');
     const regularBtn = document.querySelector('[data-testid="ptff-regular-tab-btn"]');
 
     if (charterBtn && charterBtn.classList.contains('active-btn')) {
       if (typeof ym === 'function') {
         ym(96674199, 'reachGoal', 'flight_charter');
-        obs.disconnect();
+        clearInterval(intervalId);
       }
     } else if (regularBtn && regularBtn.classList.contains('active-btn')) {
       if (typeof ym === 'function') {
         ym(96674199, 'reachGoal', 'flight_regular');
-        obs.disconnect();
+        clearInterval(intervalId);
       }
     }
-  });
-
-  observer.observe(document.body, {
-    childList: true,
-    subtree: true,
-    attributes: true,
-    attributeFilter: ['class']
-  });
+  }, 500);
 }
 
 trackFlightTypeLoad();
