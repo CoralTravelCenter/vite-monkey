e5f2b5fb3f023b0434102bdaba067f937230afb4
Mike <50363065+MikeGankin@users.noreply.github.com>
2026-08-26 17:49:04 +0300

    +++ Update

--- brands/sunmar/sunmar-ny-2027-gnb/src/scripts/prodWidget/initProdWidget.js
+++ brands/sunmar/sunmar-ny-2027-gnb/src/scripts/prodWidget/initProdWidget.js
@@ -1,21 +1,9 @@
 import {markup} from "../utils/keys.js";
-import {sendMetric} from "../utils/send_metric.js";
-import {openLink} from "../utils/openLink.js";
+
 
 export function initProdWidget() {
-    const selector = '[class*=BasicMenu_menuContainer__]';
-    const prodContainer = document.querySelector(selector);
+  const selector = '[class*=BasicMenu_menuContainer__]';
+  const prodContainer = document.querySelector(selector);
 
-    if (prodContainer && !prodContainer.querySelector(".sunmar-ny-2027-gnb")) {
-        prodContainer.insertAdjacentHTML("afterbegin", markup);
-
-        const sendMetricButton = prodContainer.querySelector("#sunmar-ny-2027-gnb__button");
-        if (sendMetricButton) {
-            const href = sendMetricButton.getAttribute("data-href");
-            sendMetricButton.addEventListener("click", () => {
-                sendMetric();
-                openLink(href);
-            })
-        }
-    }
+  if (prodContainer && !prodContainer.querySelector(".sunmar-ny-2027-gnb")) prodContainer.insertAdjacentHTML("afterbegin", markup)
 }
