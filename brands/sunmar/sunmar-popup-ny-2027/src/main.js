1300ef27946b9b5c5d5161397c814c549e363fdc
Shamber <147848650+shambeer@users.noreply.github.com>
2026-08-27 12:47:30 +0300

    popup new year 2027

--- brands/sunmar/sunmar-popup-ny-2027/src/main.js
+++ brands/sunmar/sunmar-popup-ny-2027/src/main.js
@@ -1,1 +1,12 @@
-import {markup} from "./scripts/utils/keys.js";
+import "./style.scss";
+import {initWidget} from "./scripts/initWidget.js";
+import {outputErrorMessage} from "./scripts/utils/errorMessage.js";
+
+(async function injectMetricPopap(){
+    try {
+        await initWidget();
+    }
+    catch (error) {
+        outputErrorMessage("Ошибка инициализации новогоднего попапа: ", error);
+    }
+})();
