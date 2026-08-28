d5a8ee28b254a25ec0009b7de47654fa1ff2eafb
Shamber <147848650+shambeer@users.noreply.github.com>
2026-08-28 10:20:13 +0300

    minor fix

--- brands/sunmar/sunmar-ny-2027-banner-metric/src/main.js
+++ brands/sunmar/sunmar-ny-2027-banner-metric/src/main.js
@@ -1,16 +1,13 @@
 import {waitForElement} from "@utils";
 import {outputErrorMessage} from "./scripts/utils/errorMessage.js";
 
-(async function injectMetric(){
+;(async function injectMetric(){
   const selector = '.hero.hello-bar .contenu > a';
   const metric = "ym(215233, 'reachGoal', 'entry_point', {name_stock: {NY_26_27: {name_point: 'banner',},},});";
   try {
     const container = await waitForElement(selector);
     if (container) {
       container.setAttribute("onclick", metric);
-    }
-    else {
-      console.error(`Элемент ${container} не найден`);
     }
   }
   catch(error) {
