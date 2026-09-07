7426cd2a858fdfda74ab4308cabd205155894792
Shamber <147848650+shambeer@users.noreply.github.com>
2026-09-07 16:59:17 +0300

    refactor selectors

--- brands/sunmar/sunmar-faq-hotel-reservation-additional-service/src/scripts/keys.js
+++ brands/sunmar/sunmar-faq-hotel-reservation-additional-service/src/scripts/keys.js
@@ -1,2 +1,2 @@
 export { default as MARKUP } from "../markup.html?raw";
-export const SELECTOR = '[data-testid="product-summary"]';
+export const SELECTOR = '#section-row-3:has([data-testid="test-extra-service-widget"]) #section-column-1 [data-testid="product-summary"]';
--- brands/sunmar/sunmar-faq-hotel-reservation-customer/src/scripts/keys.js
+++ brands/sunmar/sunmar-faq-hotel-reservation-customer/src/scripts/keys.js
@@ -1,2 +1,2 @@
 export { default as MARKUP } from "../markup.html?raw";
-export const SELECTOR = '[data-testid="product-summary"]';
+export const SELECTOR = '#section-row-3:has([data-testid="cf-surname-input"]) #section-column-1 [data-testid="product-summary"]';
